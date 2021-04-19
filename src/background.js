a5c4447cb3d5193a4429a7d984f181b8611b11d1
ibit <48777551+Ibit-to@users.noreply.github.com>
2021-04-19 20:50:21 +0200

    Update background.js

--- src/background.js
+++ src/background.js
@@ -4,10 +4,18 @@ const Logger = require("./Helpers/Logger");
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     Logger.debug("Received request", request);
 
-    axios
-        .get(`https://tv-v2.api-fetch.sh/${request.type}/${request.imdbID}`)
-        .then((result) => sendResponse({ data: result.data }))
-        .catch((error) => Logger.error(error));
+    if(request.type === 'movie')
+        axios
+            .get(`https://yts.mx/api/v2/list_movies.json?query_term=${request.imdbID}`)
+            .then((result) => sendResponse({ data: result.data }))
+            .catch((error) => Logger.error(error));
+    else {
+        let imdbID = request.imdbID.replace(/[a-z]+/i, '');
+        axios
+            .get(`https://eztv.re/api/get-torrents?limit=100&imdb_id=${imdbID}`)
+            .then((result) => sendResponse({data: result.data}))
+            .catch((error) => Logger.error(error));
+    }
 
     return true; // async response
 });
